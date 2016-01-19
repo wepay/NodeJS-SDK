8378abc4acd377575771aef01058bb853990ce2f
Julius D'souza <julius@wepay.com>
2016-01-19 13:24:38 -0800

    NodeJS SDK: convert response type from buffer to JSON object

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 // load in your modules
-var wepay = require('./wepay').WEPAY;
+var wepay = require('./lib/wepay.js').WEPAY;
 
-// local variables
 var wepay_settings = {
-	'client_id'     : '17742123120',
-	'client_secret' : '6280c3asdaceb6',
-	'access_token'  : 'a9asdaff4cb866893119097e0c29ee1f7886b3891e76b4599cb589c232b4f8f6ddcd', // used for oAuth2
+    'access_token'  : 'a9asdaff4cb866893119097e0c29ee1f7886b3891e76b4599cb589c232b4f8f6ddcd', // used for oAuth2
+    'client_id'     : '17742123120',
+    'client_secret' : '6280c3asdaceb6'
 }
 
 var wp = new wepay(wepay_settings);
 wp.use_staging(); // use staging environment (payments are not charged)
 
 try {
-	wp.call('/checkout/create',
-		{
-			'account_id': 1513122,
-			'short_description': 'Selling 42 Pens',
-			'type': 'GOODS',
-			'amount': 50
-		},
-		function(response) {
-			console.log('%s', response);
-		}
-	);
+    wp.call('/checkout/create',
+        {
+            'account_id': 1513122,
+            'amount': 50,
+            'currency': 'USD',
+            'short_description': 'Selling 42 Pens',
+            'type': 'goods'
+        },
+        function(response) {
+            console.log(response);
+        }
+    );
 } catch (error) {
-	console.log(error);
+    console.log(error);
 }
