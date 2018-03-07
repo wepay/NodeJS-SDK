7f0445ae6184a18c031574196258d29dfc77d9ce
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-03-06 22:22:18 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,32 @@
 // load in your modules
 var wepay = require('./lib/wepay.js').WEPAY;
 
+const WepayClient = require('./lib/wepayClient');
+
 var wepay_settings = {
-    'access_token': 'a9asdaff4cb866893119097e0c29ee1f7886b3891e76b4599cb589c232b4f8f6ddcd', // used for oAuth2
-    'client_id': '17742123120',
-    'client_secret': '6280c3asdaceb6'
-}
+  access_token: 'a9asdaff4cb866893119097e0c29ee1f7886b3891e76b4599cb589c232b4f8f6ddcd', // used for oAuth2
+  client_id: '17742123120',
+  client_secret: '6280c3asdaceb6',
+};
 
-var wp = new wepay(wepay_settings);
-wp.use_staging(); // use staging environment (payments are not charged)
+async function callWepay() {
+  const client = new WepayClient(
+    'a9asdaff4cb866893119097e0c29ee1f7886b3891e76b4599cb589c232b4f8f6ddcd'
+  );
 
-async function callThis() {
-    try {
-        const response = await wp.callPromise('/checkout/create',
-            {
-                'account_id': 1513122,
-                'amount': 50,
-                'currency': 'USD',
-                'short_description': 'Selling 42 Pens',
-                'type': 'goods'
-            });
-        console.log(response);
-    } catch (e) {
-        console.log(e);
-    }
-}
+  try {
+    const response = await client.call('/checkout/create', {
+      account_id: 1513122,
+      amount: 50,
+      currency: 'USD',
+      short_description: 'Selling 42 Pens',
+      type: 'goods',
+    });
 
-
-callThis();
-try {
-    wp.call('/checkout/create',
-        {
-            'account_id': 1513122,
-            'amount': 50,
-            'currency': 'USD',
-            'short_description': 'Selling 42 Pens',
-            'type': 'goods'
-        },
-        function (response) {
-            console.log(response);
-        }
-    );
-} catch (error) {
+    console.log(response);
+  } catch (error) {
     console.log(error);
+  }
 }
+
+callWepay();
