5cf9ab68771861d42a6c49c7730d601e82a7d63d
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-03-06 22:37:23 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 // load in your modules
-var wepay = require('./lib/wepay.js').WEPAY;
-
 const WepayClient = require('./lib/wepayClient');
 
-var wepay_settings = {
-  access_token: 'a9asdaff4cb866893119097e0c29ee1f7886b3891e76b4599cb589c232b4f8f6ddcd', // used for oAuth2
-  client_id: '17742123120',
-  client_secret: '6280c3asdaceb6',
-};
+const WEPAY_ACCESS_TOKEN = 'a9asdaff4cb866893119097e0c29ee1f7886b3891e76b4599cb589c232b4f8f6ddcd';
 
 async function callWepay() {
   const client = new WepayClient(
-    'a9asdaff4cb866893119097e0c29ee1f7886b3891e76b4599cb589c232b4f8f6ddcd'
+    WEPAY_ACCESS_TOKEN
   );
 
   try {
